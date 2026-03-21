59da9b25a81171aa05a7a56d7295f58d851899f0
vedant-asati03 <vedant.asati03@gmail.com>
2026-03-21 17:24:17 +0530

    Improved comments for better understandability in future

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 #![no_std] // attribute to not use rust standard library
-#![no_main] // we will need to implement our own entry point
+#![no_main] // we will need to implement our own entry point, so we disable all rust-level entry points
 
 use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)] // we dont want the name of this function to be mangled, so we add this trait
+pub extern "C" fn _start() -> ! {
+    // we named the function _start because the linker by-default looks for a function named
+    // _start, this function is the custom entry point we defined
+    loop {}
+}
 
 /// This function is called on panic.
 #[panic_handler]
@@ -9,7 +16,4 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
+
